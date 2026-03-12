5f92ce60e5a83d97469c9f01a9803e7f30e61363
zeyad ahmed nour <zezoflutter2025@gmail.com>
2026-03-12 03:08:16 +0200

    added image called logo2 and appbarcolor

---
+++ lib/core/utils/colors.dart
@@ -0,0 +1,8 @@
+import 'dart:ui';
+
+const Color buttonColor = Color(0xffd10100);
+const Color iconColor = Color(0xffd10100);
+const Color backgroundpageColor = Color(0xfff6f0fa);
+const Color formColor = Color(0xfffdfdfb);
+const Color cardColor = Color(0xfff6f0fa);
+const Color appbarColor = Color(0xffef1621);
