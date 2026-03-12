5470f1a58394e3433f2c1944716c5fa5096b8fb8
zeyad ahmed nour <zezoflutter2025@gmail.com>
2026-03-12 02:59:09 +0200

    inite structe project and added main color and main image

---
+++ lib/core/colors.dart
@@ -0,0 +1,7 @@
+import 'dart:ui';
+
+const Color buttonColor =Color(0xffd10100);
+const Color iconColor =Color(0xffd10100);
+const Color backgroundpageColor =Color(0xfff6f0fa);
+const Color formColor =Color(0xfffdfdfb);
+const Color cardColor =Color(0xfff6f0fa);
