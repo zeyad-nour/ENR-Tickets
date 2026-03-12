13f9b5bde7db4edc84c079532a39676a4f5d3cf5
zeyad ahmed nour <zezoflutter2025@gmail.com>
2026-03-12 03:36:40 +0200

    creaet assets folder and added som Strings and Colors

--- lib/core/utils/colors.dart
+++ lib/core/utils/colors.dart
@@ -6,3 +6,5 @@ const Color backgroundpageColor = Color(0xfff6f0fa);
 const Color formColor = Color(0xfffdfdfb);
 const Color cardColor = Color(0xfff6f0fa);
 const Color appbarColor = Color(0xffef1621);
+const Color mainfontcolor = Color(0xff060604);
+const Color hintColorForm = Color(0xff9a9695);
--- lib/features/log_in/presentation/view/widgets/log_in_body.dart
+++ lib/features/log_in/presentation/view/widgets/log_in_body.dart
@@ -1,3 +1,5 @@
+import 'package:enr_tickets/core/utils/strings.dart';
+import 'package:enr_tickets/core/utils/widget/assets.dart';
 import 'package:flutter/material.dart';
 
 class LogInBody extends StatelessWidget {
@@ -5,8 +7,18 @@ class LogInBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(children: [
-       
-    ],);
+    return Column(
+      children: [
+        Center(child: Image.asset(AssetsData.logo)),
+        Text(
+          headlogIn,
+          style: TextStyle(
+            fontSize: 27,
+            fontWeight: FontWeight.w700,
+            color: Colors.black,
+          ),
+        ),
+      ],
+    );
   }
 }
