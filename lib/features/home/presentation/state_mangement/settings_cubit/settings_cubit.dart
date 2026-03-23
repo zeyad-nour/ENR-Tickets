cd3071de74c04fe64babafac91a650ee88fcedce
zeyad ahmed nour <zezoflutter2025@gmail.com>
2026-03-23 05:24:34 +0200

    added settings cubit

---
+++ lib/features/home/presentation/state_mangement/settings_cubit/settings_cubit.dart
@@ -0,0 +1,35 @@
+import 'package:bloc/bloc.dart';
+import 'package:flutter/material.dart';
+
+part 'settings_state.dart';
+
+class SettingsCubit extends Cubit<SettingsState> {
+  SettingsCubit() : super(SettingsInitial());
+
+  /// فتح / قفل الاختيارات
+  bool isThemeOpen = false;
+
+  /// الثيم الحالي
+  ThemeMode themeMode = ThemeMode.system;
+
+  void toggleTheme() {
+    isThemeOpen = !isThemeOpen;
+    emit(SettingsChange());
+  }
+
+  void changeTheme(ThemeMode mode) {
+    themeMode = mode;
+    emit(SettingsChange());
+  }
+
+  String get themeText {
+    switch (themeMode) {
+      case ThemeMode.system:
+        return "Follow System";
+      case ThemeMode.light:
+        return "Light Mode";
+      case ThemeMode.dark:
+        return "Dark Mode";
+    }
+  }
+}
---
+++ lib/features/home/presentation/state_mangement/settings_cubit/settings_state.dart
@@ -0,0 +1,8 @@
+part of 'settings_cubit.dart';
+
+@immutable
+sealed class SettingsState {}
+
+final class SettingsInitial extends SettingsState {}
+
+final class SettingsChange extends SettingsState {}
