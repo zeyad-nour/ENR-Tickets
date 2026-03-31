247fee8338c1c74dc69238a4f9a47669a8561621
zeyad ahmed nour <zezoflutter2025@gmail.com>
2026-03-31 17:45:40 +0200

    added methods image

--- lib/features/payment_way/presentation/view/payment_way.dart
+++ lib/features/payment_way/presentation/view/payment_way.dart
@@ -1,9 +1,11 @@
 import 'package:enr_tickets/core/utils/colors.dart';
 import 'package:enr_tickets/core/widget/styles.dart';
+import 'package:enr_tickets/features/payment_way/presentation/state_mangement/cubit/payment_cubit.dart';
 import 'package:enr_tickets/features/payment_way/presentation/view/widgets/custom_divider.dart';
 import 'package:enr_tickets/features/payment_way/presentation/view/widgets/custom_text.dart';
 import 'package:enr_tickets/features/payment_way/presentation/view/widgets/custom_widget_card.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:gap/gap.dart';
 
 class PaymentWay extends StatelessWidget {
@@ -11,46 +13,59 @@ class PaymentWay extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      appBar: AppBar(
-        title: Text(
-          "Complete Payment",
-          style: Styles.textStyle20.copyWith(color: Colors.white),
+    return BlocProvider(
+      create: (_) => PaymentCubit(),
+      child: Scaffold(
+        backgroundColor: Colors.white,
+        appBar: AppBar(
+          title: Text(
+            "Complete Payment",
+            style: Styles.textStyle20.copyWith(color: Colors.white),
+          ),
+          centerTitle: true,
+          backgroundColor: appbarColor,
+          leading: IconButton(
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+            icon: Icon(Icons.arrow_back_ios_new, color: Colors.white),
+          ),
         ),
-        centerTitle: true,
-        backgroundColor: appbarColor,
-        leading: IconButton(
-          onPressed: () {
-            Navigator.of(context).pop();
-          },
-          icon: Icon(Icons.arrow_back_ios_new, color: Colors.white),
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Gap(20),
+            CustomText(
+              title: "Complete Payment",
+              stylestitle: Styles.textStyle27,
+            ),
+            Gap(20),
+            CustomDivider(),
+            Gap(20),
+            CustomText(
+              title: "350 EGP Amount Due:",
+              stylestitle: Styles.textStyle20,
+            ),
+            Gap(20),
+            CustomDivider(),
+            CustomText(
+              title: "Choose Payment Method :",
+              stylestitle: Styles.textStyle20,
+            ),
+            Gap(20),
+            CustomWidgetCard(
+              title: "Bank Cards",
+              methods: [
+                {"title": "Visa", "image": "assets/images/visa.png"},
+                {
+                  "title": "MasterCard",
+                  "image": "assets/images/mastercard.png",
+                },
+                {"title": "Meeza", "image": "assets/images/meeza.png"},
+              ],
+            ),
+          ],
         ),
-      ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Gap(20),
-          CustomText(
-            title: "Complete Payment",
-            stylestitle: Styles.textStyle27,
-          ),
-          Gap(20),
-          CustomDivider(),
-          Gap(20),
-          CustomText(
-            title: "350 EGP Amount Due:",
-            stylestitle: Styles.textStyle20,
-          ),
-          Gap(20),
-          CustomDivider(),
-          CustomText(
-            title: "Choose Payment Method :",
-            stylestitle: Styles.textStyle20,
-          ),
-          Gap(20),
-          CustomWidgetCard(title: "Bank Cards"),
-        ],
       ),
     );
   }
---
+++ lib/features/payment_way/presentation/view/widgets/payment_iteam.dart
@@ -0,0 +1,30 @@
+import 'package:enr_tickets/features/payment_way/presentation/state_mangement/cubit/payment_cubit.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+Widget paymentItem(BuildContext context, String title, String imagePath) {
+  final cubit = context.read<PaymentCubit>();
+  final selected = context.watch<PaymentCubit>().selectedMethod;
+
+  bool isSelected = selected == title;
+
+  return Expanded(
+    child: GestureDetector(
+      onTap: () => cubit.selectMethod(title),
+      child: AnimatedContainer(
+        duration: const Duration(milliseconds: 300),
+
+        child: Column(
+          children: [
+            Radio<String>(
+              value: title,
+              groupValue: selected,
+              onChanged: (v) => cubit.selectMethod(v!),
+            ),
+            Expanded(child: Image.asset(imagePath)),
+          ],
+        ),
+      ),
+    ),
+  );
+}
