eef2db6ead9f0f0b8262930671ce2cc967fc99cb
zeyad ahmed nour <zezoflutter2025@gmail.com>
2026-04-01 02:16:04 +0200

    update shared_preferences dependency to version 2.5.5 and change its status to direct main

--- lib/features/payment_way/presentation/view/widgets/payment_iteam.dart
+++ lib/features/payment_way/presentation/view/widgets/payment_iteam.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: deprecated_member_use
+
 import 'package:enr_tickets/features/payment_way/presentation/state_mangement/cubit/payment_cubit.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
