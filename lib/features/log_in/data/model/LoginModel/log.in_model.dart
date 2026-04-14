0a8f064d61d790b4b1eff2c49ef50be814753f84
zeyad ahmed nour <zezoflutter2025@gmail.com>
2026-04-14 07:16:00 +0200

    feat: refactor log-in models; remove old models and create new ones

--- lib/features/log_in/data/model/LoginModel/log.in_model.dart
+++ lib/features/log_in/data/model/LoginModel/log.in_model.dart
@@ -2,13 +2,13 @@ import 'package:equatable/equatable.dart';
 
 import 'user.in_model.dart';
 
-class Log extends Equatable {
+class LogIn extends Equatable {
   final String? token;
   final User? user;
 
-  const Log({this.token, this.user});
+  const LogIn({this.token, this.user});
 
-  factory Log.fromJson(Map<String, dynamic> json) => Log(
+  factory LogIn.fromJson(Map<String, dynamic> json) => LogIn(
     token: json['token'] as String?,
     user: json['user'] == null
         ? null
--- lib/features/log_in/data/repo/log-in_repo.dart
+++ lib/features/log_in/data/repo/log-in_repo.dart
@@ -1,1 +1,7 @@
-abstract class LogInRepo {}
+import 'package:dartz/dartz.dart';
+import 'package:enr_tickets/core/services/error/failures.dart';
+import 'package:enr_tickets/features/log_in/data/model/LoginModel/log.in_model.dart';
+
+abstract class LogInRepo { 
+  Future<Either<Failure, LogIn>> logIn({required String email, required String password});
+}
