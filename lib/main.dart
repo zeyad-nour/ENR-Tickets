226ccc7ecb10d6cef6460d39d5ef1c6352392e9d
zeyad ahmed nour <zezoflutter2025@gmail.com>
2026-04-21 05:33:27 +0200

    feat: integrate StationRepo and HomeCubit into MyApp for improved state management

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,9 @@ import 'package:enr_tickets/core/utils/local_storage.dart';
 import 'package:enr_tickets/features/create_account/data/repo/signin_repo.dart';
 import 'package:enr_tickets/features/create_account/data/repo/signin_repo_implement.dart';
 import 'package:enr_tickets/features/create_account/presentation/view/create_account.dart';
+import 'package:enr_tickets/features/home/data/repo/station_repo.dart';
+import 'package:enr_tickets/features/home/data/repo/station_repo_implement.dart';
+import 'package:enr_tickets/features/home/presentation/state_mangement/home_cubit/home_cubit.dart';
 import 'package:enr_tickets/features/log_in/data/repo/login_repo.dart';
 import 'package:enr_tickets/features/log_in/data/repo/login_repo_implement.dart';
 import 'package:enr_tickets/features/settings/presentation/state_mangement/settings_cubit/settings_cubit.dart';
@@ -26,59 +29,87 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MultiBlocProvider(
+    return MultiRepositoryProvider(
       providers: [
-        RepositoryProvider<SigninRepo>(
-          create: (_) => SigninRepoImplement(ApiService(Dio())),
-        ),
-        RepositoryProvider<LogInRepo>(
-          create: (_) => LoginRepoImplement(ApiService(Dio())),
-        ),
-        BlocProvider<SettingsCubit>(create: (_) => SettingsCubit()),
-        BlocProvider<SeatSelectionCubit>(create: (_) => SeatSelectionCubit()),
+    RepositoryProvider<SigninRepo>(
+      create: (_) => SigninRepoImplement(ApiService(Dio())),
+    ),
+    RepositoryProvider<LogInRepo>(
+      create: (_) => LoginRepoImplement(ApiService(Dio())),
+    ),
+
+    RepositoryProvider<StationRepo>(
+      create: (_) => StationRepoImplement(ApiService(Dio())),
+    ),
+
+    BlocProvider<SettingsCubit>(
+      create: (_) => SettingsCubit(),
+    ),
+
+    BlocProvider<SeatSelectionCubit>(
+      create: (_) => SeatSelectionCubit(),
+    ),
+
+    BlocProvider<HomeCubit>(
+      create: (context) => HomeCubit(
+        context.read<StationRepo>(),
+      ),
+    ),
       ],
-      child: BlocBuilder<SettingsCubit, SettingsState>(
-        builder: (context, state) {
-          final cubit = context.watch<SettingsCubit>();
+      child: MultiBlocProvider(
+        providers: [
+          RepositoryProvider<SigninRepo>(
+            create: (_) => SigninRepoImplement(ApiService(Dio())),
+          ),
+          RepositoryProvider<LogInRepo>(
+            create: (_) => LoginRepoImplement(ApiService(Dio())),
+          ),
+          BlocProvider<SettingsCubit>(create: (_) => SettingsCubit()),
+          BlocProvider<SeatSelectionCubit>(create: (_) => SeatSelectionCubit()),
+        ],
+        child: BlocBuilder<SettingsCubit, SettingsState>(
+          builder: (context, state) {
+            final cubit = context.watch<SettingsCubit>();
 
-          return MaterialApp(
-            debugShowCheckedModeBanner: false,
+            return MaterialApp(
+              debugShowCheckedModeBanner: false,
 
-            /// Language
-            locale: cubit.locale,
-            supportedLocales: const [Locale('en'), Locale('ar')],
-            localizationsDelegates: const [
-              GlobalMaterialLocalizations.delegate,
-              GlobalWidgetsLocalizations.delegate,
-              GlobalCupertinoLocalizations.delegate,
-            ],
+              /// Language
+              locale: cubit.locale,
+              supportedLocales: const [Locale('en'), Locale('ar')],
+              localizationsDelegates: const [
+                GlobalMaterialLocalizations.delegate,
+                GlobalWidgetsLocalizations.delegate,
+                GlobalCupertinoLocalizations.delegate,
+              ],
 
-            /// Theme
-            themeMode: cubit.themeMode,
-            theme: ThemeData(
-              brightness: Brightness.light,
-              fontFamily: "Quicksand",
-              scaffoldBackgroundColor: Colors.white,
-              cardColor: Colors.white,
-              iconTheme: const IconThemeData(color: Colors.black),
-              textTheme: const TextTheme(
-                bodyLarge: TextStyle(color: Colors.black),
+              /// Theme
+              themeMode: cubit.themeMode,
+              theme: ThemeData(
+                brightness: Brightness.light,
+                fontFamily: "Quicksand",
+                scaffoldBackgroundColor: Colors.white,
+                cardColor: Colors.white,
+                iconTheme: const IconThemeData(color: Colors.black),
+                textTheme: const TextTheme(
+                  bodyLarge: TextStyle(color: Colors.black),
+                ),
               ),
-            ),
-            darkTheme: ThemeData(
-              brightness: Brightness.dark,
-              fontFamily: "Quicksand",
-              scaffoldBackgroundColor: Colors.black,
-              cardColor: const Color(0xFF1E1E1E),
-              iconTheme: const IconThemeData(color: Colors.white),
-              textTheme: const TextTheme(
-                bodyLarge: TextStyle(color: Colors.white),
+              darkTheme: ThemeData(
+                brightness: Brightness.dark,
+                fontFamily: "Quicksand",
+                scaffoldBackgroundColor: Colors.black,
+                cardColor: const Color(0xFF1E1E1E),
+                iconTheme: const IconThemeData(color: Colors.white),
+                textTheme: const TextTheme(
+                  bodyLarge: TextStyle(color: Colors.white),
+                ),
               ),
-            ),
 
-            home: loggedIn ? const HomePage() : const CreateAccount(),
-          );
-        },
+              home: loggedIn ? const HomePage() : const CreateAccount(),
+            );
+          },
+        ),
       ),
     );
   }
