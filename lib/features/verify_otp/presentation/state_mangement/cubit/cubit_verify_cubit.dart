d00ec4060b7de432f1739060eeb4e575a916e402
zeyad ahmed nour <zezoflutter2025@gmail.com>
2026-05-01 15:51:07 +0300

    feat: implement CubitVerifyCubit with OTP verification and resend functionality

--- lib/features/verify_otp/presentation/state_mangement/cubit/cubit_verify_cubit.dart
+++ lib/features/verify_otp/presentation/state_mangement/cubit/cubit_verify_cubit.dart
@@ -1,10 +1,30 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:enr_tickets/features/verify_otp/data/repo/verify_repo.dart';
 
 part 'cubit_verify_state.dart';
 
 class CubitVerifyCubit extends Cubit<CubitVerifyState> {
-  CubitVerifyCubit() : super(CubitVerifyInitial());
+  final VerifyRepo verifyRepo;
 
-  
+  CubitVerifyCubit(this.verifyRepo) : super(CubitVerifyInitial());
+
+  Future<void> verifyOtp(String otp) async {
+    emit(CubitVerifyLoding());
+
+    final result = await verifyRepo.verifyOtp(otp);
+
+    result.fold(
+      (failure) {
+        emit(CubitVerifyFailure(errorMessage: failure.errorMessage));
+      },
+      (_) {
+        emit(CubitVerifySucsess());
+      },
+    );
+  }
+
+  void resendOtp() {
+    emit(CubitVerifyResend());
+  }
 }
